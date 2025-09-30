a46eb9aef4bee0e48d942ec4f7e87ec9abadd587
Joseph Mazzini <jrogue.mazz@gmail.com>
2025-09-30 11:09:19 -0700

    Bump version to 6.0.0 and update Debian packaging

--- debian/sys-scan-graph/usr/include/sys-scan/core/JsonUtil.h
+++
@@ -1,8 +0,0 @@
-// JSON utility helpers (escaped string + ISO8601 time)
-#pragma once
-#include <string>
-#include <chrono>
-namespace sys_scan { namespace jsonutil {
-std::string escape(const std::string& s);
-std::string time_to_iso(std::chrono::system_clock::time_point tp);
-} }
