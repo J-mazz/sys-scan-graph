d1cf68d73570ac8bf3944844e77589c3ffef20f5
Joseph Mazzini <joseph@mazzlabs.works>
2025-12-25 11:14:13 -0800

    Update __init__.py
    Signed-off-by: Joseph Mazzini  <joseph@mazzlabs.works>

--- agent/sys_scan_agent/__init__.py
+++ agent/sys_scan_agent/__init__.py
@@ -1,19 +1,4 @@
-#    .________      ._____.___ .______  .______ .______ .___ .______  .___
-#    :____.   \     :         |:      \ \____  |\____  |: __|:      \ : __|
-#     __|  :/ |     |   \  /  ||   .   |/  ____|/  ____|| : ||       || : |
-#    |     :  |     |   |\/   ||   :   |\      |\      ||   ||   |   ||   |
-#     \__. __/      |___| |   ||___|   | \__:__| \__:__||   ||___|   ||   |
-#        :/               |___|    |___|    :       :   |___|    |___||___|
-#        :                                  •       •                 
-#                                                                          
-#
-#    2925
-#    __init__.py
-
-# ==============================================================================
 # Agent package init
 
-# Removed import-time subprocess check to avoid process termination on library import
-# Binary presence check moved to CLI runtime for proper error handling
-
+from .model_loader import get_model_path
 __all__ = []
